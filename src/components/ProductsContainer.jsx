e1005556bd65f8d55817cede4afa4dc2c75c8444
Maksat Geldiyev <maksat363132@gmail.com>
2025-04-14 22:52:25 +0400

    Add product count display and layout toggle (grid/list) buttons

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -7,12 +7,54 @@ import { BsFillGridFill, BsList } from "react-icons/bs";
 const ProductsContainer = () => {
   const { meta } = useLoaderData();
   console.log(meta);
-  const paginationTotal = meta.pagination.total;
+  const totalProducts = meta.pagination.total;
+
+  const [isGridView, setIsGridView] = useState("grid");
+
+  const setActiveStyles = (pattern) => {
+    return `text-xl btn btn-circle btn-sm ${
+      pattern === isGridView
+        ? "btn-primary text-primary-content"
+        : "btn-ghost text-based-content"
+    }`;
+  };
 
   return (
     <>
-      <ProductLists />
-      <ProductsGrid />
+      {/* Header */}
+      <div className="flex justify-between items-center mt-8 border-b border-base-300 pb-5">
+        <h4 className="font-medium text-md">
+          {totalProducts} product{totalProducts > 1 && "s"} found
+        </h4>
+
+        {/* View buttons */}
+        <div className="flex gap-x-2 ">
+          <button
+            type="button"
+            className={setActiveStyles("grid")}
+            onClick={() => setIsGridView("grid")}
+          >
+            <BsFillGridFill />
+          </button>
+          <button
+            type="button"
+            className={setActiveStyles("list")}
+            onClick={() => setIsGridView("list")}
+          >
+            <BsList />
+          </button>
+        </div>
+      </div>
+      {/* Products */}
+      <div>
+        {totalProducts === 0 ? (
+          <h5 className="text-2xl mt-16">Sorry, no products were found</h5>
+        ) : isGridView === "grid" ? (
+          <ProductsGrid />
+        ) : (
+          <ProductLists />
+        )}
+      </div>
     </>
   );
 };
